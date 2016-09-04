40d7d98d8ac215264887d4f93d1514313a4b07d0
Geoffrey Mainland <mainland@drexel.edu>
2016-09-04 19:22:27 -0400

    Cache write to virtual arrays.

--- Spiral/Array/Repr/Virtual.hs
+++ Spiral/Array/Repr/Virtual.hs
@@ -21,31 +21,33 @@ import qualified Data.Vector.Mutable as MV
 
 import Spiral.Array
 import Spiral.Array.Program
+import Spiral.Exp
 
 -- | Type tag for a virtual array.
 data V
 
-instance Shape sh => IsArray V sh e where
+instance Shape sh => IsArray V sh (Exp a) where
     -- | A matrix whose entries are manifest.
-    data Array V sh e = V sh (MV.IOVector e)
+    data Array V sh (Exp a) = V sh (MV.IOVector (Exp a))
 
     extent (V sh _) = sh
 
-instance Shape sh => MArray V sh e where
+instance (Shape sh, Typed a) => MArray V sh (Exp a) where
     read  (V sh v) cix = do
         ix <- fromExpShape cix
-        MV.read  v (toIndex sh ix)
+        MV.read v (toIndex sh ix)
 
-    write (V sh v) cix = do
+    write (V sh v) cix e = do
         ix <- fromExpShape cix
-        MV.write v (toIndex sh ix)
+        e' <- cache e
+        MV.write v (toIndex sh ix) e'
 
 -- | Replicate the given value according to the specified shape to produce a
 -- virtual array.
-replicateV :: (Shape sh, MonadP m) => sh -> a -> m (Array V sh a)
+replicateV :: (Shape sh, MonadP m) => sh -> Exp a -> m (Array V sh (Exp a))
 replicateV sh x = V sh <$> MV.replicate (size sh) x
 
-freezeV :: MonadP m => Array V sh a -> m (Array M sh a)
+freezeV :: MonadP m => Array V sh (Exp a) -> m (Array M sh (Exp a))
 freezeV (V sh mv) = do
     v <- V.freeze mv
     return $ M sh v
