807caba3597441696e7ddd6783d58e9c30fee1c0
Geoffrey Mainland <mainland@drexel.edu>
2016-09-06 11:05:39 -0400

    Generalize the code generator's type.

--- examples/DFTGen.hs
+++ examples/DFTGen.hs
@@ -2,12 +2,18 @@
 
 module Main (main) where
 
+import Data.Complex
+
 import Spiral.Driver
+import Spiral.Exp
 import Spiral.FFT
+import Spiral.SPL
 
 main :: IO ()
 main = defaultMain $ \args -> do
     n <- case args of
            [s] -> return (read s)
            _   -> return 4
-    codegenC ("dft_" ++ show n) (f n)
+    let dft_n :: SPL (Exp (Complex Double))
+        dft_n = f n
+    codegenC ("dft_" ++ show n) dft_n
