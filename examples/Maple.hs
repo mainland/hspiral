112ca8ef76425891703302c0d8ffaf520d904087
Geoffrey Mainland <mainland@drexel.edu>
2017-04-17 22:30:59 -0400

    Add missing file.

---
+++ examples/Maple.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main (main) where
+
+import Data.Complex
+import Text.PrettyPrint.Mainland
+import Control.Monad.IO.Class (liftIO)
+
+import Spiral
+import Spiral.Exp
+import Spiral.FFT
+import Spiral.SPL
+import Spiral.Util.MaplePretty
+
+main :: IO ()
+main = defaultMain $ \args -> do
+    n <- case args of
+           [s] -> return (read s)
+           _   -> return 4
+    let dft_n :: SPL (Exp (Complex Double))
+        dft_n = f n
+    --liftIO $ check dft_n
+    liftIO $ putDocLn $ text "with(LinearAlgebra);"
+    liftIO $ putDocLn $ text "A :=" <+> pprm (DFT n :: SPL (Exp (Complex Double))) <> semi
+    liftIO $ putDocLn $ text "B :=" <+> pprm dft_n <> semi
