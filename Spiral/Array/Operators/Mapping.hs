c1acb4286dccf4814039b3483bf5c8d34b947182
Geoffrey Mainland <mainland@drexel.edu>
2017-05-08 15:00:19 -0400

    Flesh out index operations and documentation.

--- Spiral/Array/Operators/Mapping.hs
+++ Spiral/Array/Operators/Mapping.hs
@@ -3,11 +3,12 @@
 
 -- |
 -- Module      :  Spiral.Array.Operators.Mapping
--- Copyright   :  (c) 2016 Drexel University
+-- Copyright   :  (c) 2016-2017 Drexel University
 -- License     :  BSD-style
 -- Maintainer  :  mainland@drexel.edu
 
 module Spiral.Array.Operators.Mapping (
+    mapI,
     mapS,
 
     zipWithI,
@@ -20,15 +21,31 @@ import Prelude hiding ((!!))
 import Spiral.Array
 import Spiral.Exp
 
-mapS :: (Shape sh, SArray r sh (Exp a))
+-- | Map a function over the elements of an array, returning a delayed
+-- array.
+mapI :: forall sh r a b . (Shape sh, IArray r sh (Exp a))
+     => (Exp a -> Exp b)
+     -> Array r sh (Exp a)
+     -> Array D sh (Exp b)
+mapI f a = fromFunction (extent a) g
+  where
+    g :: sh -> Exp b
+    g ix = f (index a ix)
+
+-- | Map a function over the elements of an array, returning a symbolic delayed
+-- array.
+mapS :: forall sh r a b . (Shape sh, SArray r sh (Exp a))
      => (Exp a -> Exp b)
      -> Array r sh (Exp a)
      -> Array DS sh (Exp b)
 mapS f a = fromSFunction (extent a) g
   where
+    g :: ExpShapeOf sh -> Exp b
     g ix = f (indexS a ix)
 
-zipWithI :: ( IArray r1 sh (Exp a)
+-- | Zip a function over the elements of an array, returning a delayed array.
+zipWithI :: forall sh r1 r2 a b c .
+            ( IArray r1 sh (Exp a)
             , IArray r2 sh (Exp b)
             )
          => (Exp a -> Exp b -> Exp c)
@@ -37,9 +54,13 @@ zipWithI :: ( IArray r1 sh (Exp a)
          -> Array D  sh (Exp c)
 zipWithI f a b = fromFunction (intersectDim (extent a) (extent b)) g
   where
+    g :: sh -> Exp c
     g ix = f (index a ix) (index b ix)
 
-zipWithS :: ( Shape sh
+-- | Zip a function over the elements of an array, returning a symbolic delayed
+-- array.
+zipWithS :: forall sh r1 r2 a b c .
+            ( Shape sh
             , SArray r1 sh (Exp a)
             , SArray r2 sh (Exp b)
             )
@@ -49,6 +70,7 @@ zipWithS :: ( Shape sh
          -> Array DS sh (Exp c)
 zipWithS f a b = fromSFunction (intersectDim (extent a) (extent b)) g
   where
+    g :: ExpShapeOf sh -> Exp c
     g ix = f (indexS a ix) (indexS b ix)
 
 infixl 6 .+, .-
@@ -62,6 +84,11 @@ infixl 7 .*
                  => Array r1 sh (Exp a)
                  -> Array r2 sh (Exp a)
                  -> Array DS sh (Exp a)
+-- | Point-wise addition of two arrays. Returns a symbolic delayed array.
 (.+) = zipWithS (+)
+
+-- | Point-wise subtraction of two arrays. Returns a symbolic delayed array.
 (.-) = zipWithS (-)
+
+-- | Point-wise multiplication of two arrays. Returns a symbolic delayed array.
 (.*) = zipWithS (*)
