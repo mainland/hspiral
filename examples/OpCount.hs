1e6e4165b376c7351dbff0ca3591ef9b3e7a8cd2
Geoffrey Mainland <mainland@drexel.edu>
2017-05-23 02:59:19 -0400

    Enable C codegen when counting operations.

--- examples/OpCount.hs
+++ examples/OpCount.hs
@@ -1,20 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Complex (Complex)
+import Data.Foldable (toList)
 import Text.PrettyPrint.Mainland
 import Text.PrettyPrint.Mainland.Class
 
 import Spiral
+import Spiral.Backend.C
 import Spiral.Config
+import Spiral.Driver
 import Spiral.Exp
 import Spiral.FFT.CooleyTukey
 import Spiral.OpCount
 import Spiral.Program
 import Spiral.SPL
 import Spiral.SPL.Run
+import Spiral.Util.Uniq
 
 main :: IO ()
 main = defaultMain $ \args -> do
@@ -28,10 +33,16 @@ main = defaultMain $ \args -> do
       then toProgram "f" dft_n >>= go
       else toProgram "f" (Re dft_n) >>= go
   where
-    go :: Typed a => Program a -> Spiral ()
+    go :: (Typed a, Num (Exp a)) => Program a -> Spiral ()
     go prog = do
       pprint prog
       ops <- countProgramOps prog
+      resetUnique
+      defs <- evalCg $ cgProgram prog
+      outp <- asksConfig output
+      case outp of
+        Nothing -> return ()
+        Just{}  -> writeOutput (toList defs)
       liftIO $ putDocLn $
           text "Multiplications:" <+> ppr (mulOps ops) </>
           text "      Additions:" <+> ppr (addOps ops) </>
