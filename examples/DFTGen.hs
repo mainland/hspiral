87a285a738bd0408303341db6075a62bfe86ec40
Geoffrey Mainland <mainland@drexel.edu>
2017-06-08 19:11:32 -0400

    Use generic search for dftgen.

--- examples/DFTGen.hs
+++ examples/DFTGen.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Complex (Complex)
 import Data.Foldable (toList)
+import Data.Typeable (Typeable)
 import Text.PrettyPrint.Mainland
 import Text.PrettyPrint.Mainland.Class
 
@@ -17,11 +15,14 @@ import Spiral.Config
 import Spiral.Driver
 import Spiral.Exp
 import Spiral.FFT.CooleyTukey
+import Spiral.Monad
 import Spiral.OpCount
 import Spiral.Program
 import Spiral.RootOfUnity
 import Spiral.SPL
 import Spiral.SPL.Run
+import Spiral.Search.Monad
+import Spiral.Search.Generic
 import Spiral.Util.Uniq
 
 main :: IO ()
@@ -30,7 +31,7 @@ main = defaultMain $ \args -> do
     n <- case args of
            [s] -> return (read s)
            _   -> return 4
-    let f = formula n
+    f <- formula n
     pprint f
     if useComplexType
       then toProgram "f" f >>= go
@@ -52,41 +53,11 @@ main = defaultMain $ \args -> do
           text "          Total:" <+> ppr (allOps ops)
 
 -- The SPL formula for which we generate code and count operations.
-formula :: Int -> SPL (Exp (Complex Double))
-formula n = flattenDFT splitRadix (DFT n)
-
-flattenDFT :: (Typed a, Num (Exp a))
-           => (forall a . RootOfUnity a => Int -> a -> SPL a)
-           -> SPL (Exp a)
-           -> SPL (Exp a)
-flattenDFT f e = go e
+formula :: MonadSpiral m => Int -> m (SPL (Exp (Complex Double)))
+formula n = runSearch f (DFT n)
   where
-    go :: (Typed a, Num (Exp a))
-       => SPL (Exp a)
-       -> SPL (Exp a)
-    go e@(F n w)
-      | n <= 2    = e
-      | otherwise = go $ f n w
-
-    go (Kron e1 e2) =
-        Kron (go e1) (go e2)
-
-    go (DSum e1 e2) =
-        DSum (go e1) (go e2)
-
-    go (Prod e1 e2) =
-        Prod (go e1) (go e2)
-
-    go e@E{}     = e
-    go e@Diag{}  = e
-    go e@KDiag{} = e
-    go e@Circ{}  = e
-    go e@Toep{}  = e
-    go e@I{}     = e
-    go e@Rot{}   = e
-    go e@Pi{}    = e
-    go e@F2{}    = e
-    go (Re e)    = Re (go e)
-    go (DFT n)   = go $ F n (omega n)
-    go (DFT' n)  = go $ F' n (omega n)
-    go (F' n w)  = go $ KDiag n (1/fromIntegral n) × F n (1/w)
+    f :: (Typeable a, Typed a, RootOfUnity (Exp a), MonadSpiral m)
+      => Int
+      -> Exp a
+      -> S m (SPL (Exp a))
+    f n w = search f (splitRadix n w)
