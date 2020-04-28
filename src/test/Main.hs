223c7b31143ca8e06d323b5628573137720677db
Geoffrey Mainland <mainland@drexel.edu>
2020-04-28 15:06:54 -0400

    Clean up test suite.

--- src/test/Main.hs
+++ src/test/Main.hs
@@ -23,20 +23,19 @@ import Data.Typeable (Typeable)
 import qualified Data.Vector.Storable as V
 import Data.Word (Word32)
 import GHC.TypeLits (KnownNat,
-                     Nat,
                      natVal)
-import Test.HUnit ((@?=))
+import Test.HUnit ((@?=),
+                   Assertion)
 import Test.Hspec
 import Test.Hspec.Core.Spec (Example(..),
                              Result)
-import Test.Hspec.QuickCheck
-import Test.QuickCheck (Arbitrary(..),
+import Test.QuickCheck ((===),
+                        Arbitrary(..),
                         Gen,
                         Property,
-                        choose,
                         counterexample,
-                        forAll,
-                        ioProperty)
+                        forAll)
+import Test.QuickCheck.Modifiers (Positive(..))
 
 import qualified Data.FlagSet as FS
 
@@ -70,22 +69,21 @@ main = do
 
 spec :: Spec
 spec = do
+    splTests
+    factorizationTests
+    opCountTests
+    codegenTests mempty
+
+--
+-- SPL operation test
+--
+
+splTests :: Spec
+splTests = describe "SPL operations" $ do
     strideTest
     l82Test
     kroneckerTest
     directSumTest
-    dftTests
-    searchTests
-    moddftTests
-    splitRadixOpcountTests
-    difOpcountTests
-    let conf = mempty
-    ditCodegenTests conf
-    difCodegenTests conf
-    splitRadixCodegenTests conf
-    improvedSplitRadixCodegenTests conf
-    searchCodegenTests conf
-    modCodegenTests conf
 
 -- See:
 --   https://en.wikipedia.org/wiki/Kronecker_product
@@ -149,26 +147,32 @@ directSumTest = it "Direct sum (⊕)" $
                 [0, 0, 0, 1, 6],
                 [0, 0, 0, 0, 1]]
 
-dftTests :: Spec
-dftTests = describe "DFT tests" $ do
+factorizationTests :: Spec
+factorizationTests = describe "Factorization" $ do
     f2Test
     f4Test
     f8Test
-    ck_dit_5_7_test
-    ck_dif_5_7_test
-    gt_5_7_test
-    rader_test 7
-    rader_test 23
-    bluestein_test 3 6
-    bluestein_test 4 8
-    bluestein_test 4 9
-    bluestein_test 9 18
-    describe "DIT" $ sequence_ [dit_test n | n <- [1..7]]
-    describe "DIF" $ sequence_ [dif_test n | n <- [1..7]]
-    describe "Split Radix" $ sequence_ [split_radix_test n | n <- [1..3]]
-    describe "Split Radix 8" $ sequence_ [split_radix8_test n | n <- [1..2]]
-    describe "Conjugate Pair Split Radix" $ sequence_ [conj_pair_split_radix_test n | n <- [1..3]]
-    describe "Improved Split Radix" $ sequence_ [improved_split_radix_test n | n <- [1..3]]
+    describe "Cooley-Tukey" $ do
+        cooleyTukeyDITTest 5 7
+        cooleyTukeyDIFTest 5 7
+    describe "Good-Thomas" $
+        goodThomasTest 5 7
+    describe "Rader" $ do
+        raderTest 7
+        raderTest 23
+    describe "Bluestein" $ do
+        bluesteinTest 3 6
+        bluesteinTest 4 8
+        bluesteinTest 4 9
+        bluesteinTest 9 18
+    describe "DIT" $ sequence_ [ditTest n | n <- [1..7]]
+    describe "DIF" $ sequence_ [difTest n | n <- [1..7]]
+    describe "Split Radix" $ sequence_ [splitRadixTest n | n <- [1..3]]
+    describe "Split Radix 8" $ sequence_ [splitRadix8Test n | n <- [1..2]]
+    describe "Conjugate Pair Split Radix" $ sequence_ [conjPairSplitRadixTest n | n <- [1..3]]
+    xdescribe "Improved Split Radix" $ sequence_ [improvedSplitRadixTest n | n <- [1..3]]
+    searchTests
+    modularDFTTests
 
 -- $F_2$
 f2Test :: Spec
@@ -211,89 +215,102 @@ f8Test = it "F_8" $ toMatrix (dit 8) @?= f8
 
         w = omega 8
 
--- Test Cooley-Tukey with factors 5 and 7
-ck_dit_5_7_test :: Spec
-ck_dit_5_7_test = it "CooleyTukeyDIT(5,7)" $
-    toMatrix (cooleyTukeyDIT 5 7 w) @?= toMatrix (F 35 w)
+-- Test Cooley-Tukey DIT
+cooleyTukeyDITTest :: Int -> Int -> Spec
+cooleyTukeyDITTest r s =
+    it ("CooleyTukeyDIT(" ++ show r ++ "," ++ show s ++ ")") $
+    toMatrix (cooleyTukeyDIT r s w) @?= toMatrix (F rs w)
   where
     w :: Exp (Complex Double)
-    w = omega 35
+    w = omega rs
 
-ck_dif_5_7_test :: Spec
-ck_dif_5_7_test = it "CooleyTukeyDIF(5,7)" $
-    toMatrix (cooleyTukeyDIF 5 7 w) @?= toMatrix (F 35 w)
+    rs :: Int
+    rs = r*s
+
+-- Test Cooley-Tukey DIF
+cooleyTukeyDIFTest :: Int -> Int -> Spec
+cooleyTukeyDIFTest r s =
+    it ("CooleyTukeyDIF(" ++ show r ++ "," ++ show s ++ ")") $
+    toMatrix (cooleyTukeyDIF r s w) @?= toMatrix (F rs w)
   where
     w :: Exp (Complex Double)
-    w = omega 35
+    w = omega rs
+
+    rs :: Int
+    rs = r*s
 
 -- Test Good-Thomas with factors 5 and 7
-gt_5_7_test :: Spec
-gt_5_7_test = it "GoodThomas(5,7)" $
-    toMatrix (goodThomas 5 7 w) @?= toMatrix (F 35 w)
+goodThomasTest :: Int -> Int -> Spec
+goodThomasTest r s =
+    it ("GoodThomas(" ++ show r ++ "," ++ show s ++ ")") $
+    toMatrix (goodThomas r s w) @?= toMatrix (F rs w)
   where
     w :: Exp (Complex Double)
-    w = omega 35
+    w = omega rs
+
+    rs :: Int
+    rs = r*s
 
 -- Test Rader for given prime
-rader_test :: Int -> Spec
-rader_test n = it ("Rader(" ++ show n ++ ")") $
+raderTest :: Int -> Spec
+raderTest n = it ("Rader(" ++ show n ++ ")") $
     toMatrix (rader (fromIntegral n) w) @?= toMatrix (F n w)
   where
     w :: Exp (Complex Double)
     w = omega n
 
 -- Test Bluestein for given n and m
-bluestein_test :: Int -> Int -> Spec
-bluestein_test n m = it ("Bluestein(" ++ show n ++ "," ++ show m ++ ")") $
+bluesteinTest :: Int -> Int -> Spec
+bluesteinTest n m = it ("Bluestein(" ++ show n ++ "," ++ show m ++ ")") $
     toMatrix (bluestein n m w) @?= toMatrix (DFT n)
   where
     w :: Exp (Complex Double)
     w = omega (2*n)
 
 -- DIT test
-dit_test :: Int -> Spec
-dit_test n = it ("DIT(2^" ++ show n ++ ")") $
+ditTest :: Int -> Spec
+ditTest n = it ("DIT(2^" ++ show n ++ ")") $
     toMatrix fft_spl @?= toMatrix (DFT (2^n))
   where
     fft_spl :: SPL (Exp (Complex Double))
     fft_spl = dit (2^n)
 
 -- DIF test
-dif_test :: Int -> Spec
-dif_test n = it ("DIF(2^" ++ show n ++ ")") $
+difTest :: Int -> Spec
+difTest n = it ("DIF(2^" ++ show n ++ ")") $
     toMatrix fft_spl @?= toMatrix (DFT (2^n))
   where
     fft_spl :: SPL (Exp (Complex Double))
     fft_spl = dif (2^n)
 
 -- Split radix test
-split_radix_test :: Int -> Spec
-split_radix_test n = it ("SplitRadix(4^" ++ show n ++ ")") $
+splitRadixTest :: Int -> Spec
+splitRadixTest n = it ("SplitRadix(4^" ++ show n ++ ")") $
     toMatrix (splitRadix (4^n) w) @?= toMatrix (DFT (4^n))
   where
     w :: Exp (Complex Double)
     w = omega (4^n)
 
 -- Split radix 8 test
-split_radix8_test :: Int -> Spec
-split_radix8_test n = it ("SplitRadix(8^" ++ show n ++ ")") $
+splitRadix8Test :: Int -> Spec
+splitRadix8Test n = it ("SplitRadix(8^" ++ show n ++ ")") $
     toMatrix (splitRadix8 (8^n) w) @?= toMatrix (DFT (8^n))
   where
     w :: Exp (Complex Double)
     w = omega (8^n)
 
 -- Split radix test
-conj_pair_split_radix_test :: Int -> Spec
-conj_pair_split_radix_test n = it ("ConjPairSplitRadix(4^" ++ show n ++ ")") $
+conjPairSplitRadixTest :: Int -> Spec
+conjPairSplitRadixTest n = it ("ConjPairSplitRadix(4^" ++ show n ++ ")") $
     toMatrix (conjPairSplitRadix (4^n) w) @?= toMatrix (DFT (4^n))
   where
     w :: Exp (Complex Double)
     w = omega (4^n)
 
 -- Split radix test
-improved_split_radix_test :: Int -> Spec
-improved_split_radix_test n = it ("ImprovedSplitRadix(4^" ++ show n ++ ")") $
-    toMatrix (conjPairSplitRadix (4^n) w) @?= toMatrix (DFT (4^n))
+improvedSplitRadixTest :: Int -> Spec
+improvedSplitRadixTest n = it ("ImprovedSplitRadix(4^" ++ show n ++ ")") $
+    toMatrix (improvedSplitRadix (4^n) w) @?= toMatrix (DFT (4^n))
   where
     w :: Exp (Complex Double)
     w = omega (4^n)
@@ -313,48 +330,33 @@ searchTests =
 -- Modular DFT tests
 --
 
-moddftTests :: Spec
-moddftTests = describe "Modular DFT tests" $ do
-    modf4Test_17
-    modf128Test_2013265921
+modularDFTTests :: Spec
+modularDFTTests = describe "Modular DFT" $ do
+    mkModularDFTTest (Proxy :: Proxy 17) 4
+    mkModularDFTTest (Proxy :: Proxy 2013265921) 128
 
--- $F_4$
-modf4Test_17 :: Spec
-modf4Test_17 = it "F_4 in ℤ/17" $
-    toMatrix fft_spl @?= toMatrix (DFT 4)
+mkModularDFTTest :: forall p . KnownNat p
+                 => Proxy p
+                 -> Int
+                 -> Spec
+mkModularDFTTest proxy n = it ("F_" ++ show n ++ " in ℤ/" ++ show p) $
+    toMatrix fft_spl @?= toMatrix (DFT n)
   where
-    fft_spl :: SPL (Exp (ℤ/17))
-    fft_spl = dit 4
+    p :: Integer
+    p = natVal proxy
 
--- $F_64$
-modf128Test_2013265921 :: Spec
-modf128Test_2013265921 = it "F_128 in ℤ/2013265921" $
-    toMatrix fft_spl @?= toMatrix (DFT 128)
-  where
-    fft_spl :: SPL (Exp (ℤ/2013265921))
-    fft_spl = dit 128
+    fft_spl :: SPL (Exp (ℤ/p))
+    fft_spl = dit n
 
-splitRadixOpcountTests :: Spec
-splitRadixOpcountTests =
-    describe "Split radix opcounts" $
-    sequence_ [mkTest n (muls + adds) | (n, muls, adds) <- splitRadixOpcounts]
-  where
-    mkTest :: Int -> Int -> Spec
-    mkTest n nops =
-      opCountTest ("Split radix " ++ show n) fs nops (runSearch () f (DFT n))
-      where
-        fs :: [DynFlag]
-        fs = [ StoreIntermediate
-             , SplitComplex
-             , CSE
-             , Rewrite
-             ]
+--
+-- Operation count tests
+--
 
-    f :: (Typeable a, Typed a, MonadSpiral m)
-      => SPL (Exp a)
-      -> S s m (SPL (Exp a))
-    f (F n w) = splitRadixBreakdown n w
-    f _       = mzero
+opCountTests :: Spec
+opCountTests =
+    describe "Opcount" $ do
+    splitRadixOpcountTests
+    difOpcountTests
 
 -- The number of multiplies and additions for spit radix decomposition of size n
 -- when using three-multiply form of complex multiply. Taken from Table II of
@@ -373,6 +375,30 @@ splitRadixOpcounts = [ (4,    0,     16)
                      --, (4096, 36868, 135172)
                      ]
 
+splitRadixOpcountTests :: Spec
+splitRadixOpcountTests =
+    describe "Split radix opcounts" $
+    sequence_ [mkTest n (muls + adds) | (n, muls, adds) <- splitRadixOpcounts]
+  where
+    mkTest :: Int -> Int -> Spec
+    mkTest n nops =
+      mkOpCountTest ("Split radix " ++ show n) fs nops (runSearch () f (DFT n))
+      where
+        fs :: [DynFlag]
+        fs = [ StoreIntermediate
+             , SplitComplex
+             , CSE
+             , Rewrite
+             ]
+
+    f :: (Typeable a, Typed a, MonadSpiral m)
+      => SPL (Exp a)
+      -> S s m (SPL (Exp a))
+    f (F n w) = splitRadixBreakdown n w
+    f _       = mzero
+
+-- The DIF form should have the same operation count as split radix when the
+-- DifRewrite flag is enabled.
 difOpcountTests :: Spec
 difOpcountTests =
     describe "DIF opcounts" $
@@ -380,7 +406,7 @@ difOpcountTests =
   where
     mkTest :: Int -> Int -> Spec
     mkTest n nops =
-      opCountTest ("DIF " ++ show n) fs nops (return $ dif n)
+      mkOpCountTest ("DIF " ++ show n) fs nops (return $ dif n)
       where
         fs :: [DynFlag]
         fs = [ StoreIntermediate
@@ -390,38 +416,52 @@ difOpcountTests =
              , DifRewrite
              ]
 
+mkOpCountTest :: String
+              -> [DynFlag]
+              -> Int
+              -> Spiral (SPL (Exp (Complex Double)))
+              -> Spec
+mkOpCountTest desc fs nops gen =
+    it desc $ do
+    ops <- runSpiralWith mempty $ withOpcountFlags fs $ do
+           f   <- gen
+           toProgram "f" (Re f) >>= countProgramOps
+    return $ mulOps ops + addOps ops @?= nops
+
 withOpcountFlags :: MonadConfig m => [DynFlag] -> m a -> m a
 withOpcountFlags fs =
     localConfig $ \env -> env { dynFlags  = FS.fromList fs
                               , maxUnroll = 256
                               }
 
-opCountTest :: String
-            -> [DynFlag]
-            -> Int
-            -> Spiral (SPL (Exp (Complex Double)))
-            -> Spec
-opCountTest desc fs nops gen = do
-    ops <- runIO $ runSpiralWith mempty $ withOpcountFlags fs $ do
-           f <- gen
-           toProgram "f" (Re f) >>= countProgramOps
-    it desc $ mulOps ops + addOps ops @?= nops
-    return ()
+--
+-- Code generation tests
+--
+
+codegenTests :: Config -> Spec
+codegenTests conf =
+    describe "Code Generation Tests" $ do
+    ditCodegenTests conf
+    difCodegenTests conf
+    splitRadixCodegenTests conf
+    improvedSplitRadixCodegenTests conf
+    searchCodegenTests conf
+    modCodegenTests conf
 
 ditCodegenTests :: Config -> Spec
 ditCodegenTests conf =
     describe "Generated DIT" $
-    codegenTests conf "Generated DIT DFT of size" (\n -> return (Re (dit n)))
+    mkCodegenTests conf "Generated DIT DFT of size" (return . Re . dit)
 
 difCodegenTests :: Config -> Spec
 difCodegenTests conf =
     describe "Generated DIF" $
-    codegenTests conf "Generated DIF DFT of size" (\n -> return (Re (dif n)))
+    mkCodegenTests conf "Generated DIF DFT of size" (return . Re . dif)
 
 splitRadixCodegenTests :: Config -> Spec
 splitRadixCodegenTests conf =
     describe "Generated splitRadix DFT" $
-    codegenTests conf "Split radix DFT of size" (\n -> runSearch () f (Re (DFT n)))
+    mkCodegenTests conf "Split radix DFT of size" (runSearch () f . Re . DFT)
   where
     f :: (Typeable a, Typed a, MonadSpiral m)
       => SPL (Exp a)
@@ -432,7 +472,7 @@ splitRadixCodegenTests conf =
 improvedSplitRadixCodegenTests :: Config -> Spec
 improvedSplitRadixCodegenTests conf =
     xdescribe "Generated improved split radix DFT" $
-    codegenTests conf "Improved split radix DFT of size" (\n -> runSearch () f (Re (DFT n)))
+    mkCodegenTests conf "Improved split radix DFT of size" (runSearch () f . Re . DFT)
   where
     f :: (Typeable a, Typed a, MonadSpiral m)
       => SPL (Exp a)
@@ -443,7 +483,7 @@ improvedSplitRadixCodegenTests conf =
 searchCodegenTests :: Config -> Spec
 searchCodegenTests conf =
     describe "Generated opcount-optimized DFT" $
-    codegenTests conf "Opcount-optimized DFT of size" (\n -> searchOpCount (Re (DFT n)))
+    mkCodegenTests conf "Opcount-optimized DFT of size" (searchOpCount . Re . DFT)
 
 instance Example (IO Property) where
     type Arg (IO Property) = ()
@@ -451,16 +491,22 @@ instance Example (IO Property) where
         p <- mp
         evaluateExample p c action progressCallback
 
+instance Example (IO Assertion) where
+    type Arg (IO Assertion) = ()
+    evaluateExample ma c action progressCallback = do
+        a <- ma
+        evaluateExample a c action progressCallback
+
 instance Example ((Property -> IO Result) -> IO Result) where
     type Arg ((Property -> IO Result) -> IO Result) = ()
     evaluateExample k c action progressCallback =
         k $ \p -> evaluateExample p c action progressCallback
 
-codegenTests :: Config
-             -> String
-             -> (Int -> Spiral (SPL (Exp Double)))
-             -> Spec
-codegenTests conf desc f =
+mkCodegenTests :: Config
+               -> String
+               -> (Int -> Spiral (SPL (Exp Double)))
+               -> Spec
+mkCodegenTests conf desc f =
     sequence_ [dftTest (2^i) | i <- [1..9::Int]]
   where
     dftTest :: Int -> Spec
@@ -469,63 +515,56 @@ codegenTests conf desc f =
         withComplexTransform conf ("dft" ++ show n) e $ \dft ->
           k $ forAll (uniformVectorsOfSize n) $ \v -> epsDiff (dft v) (FFTW.fft n v)
 
-data ModTest (p :: Nat) = ModTest Int Int
-  deriving (Eq, Ord, Show)
-
-instance Arbitrary (ModTest 2013265921) where
-    arbitrary = do
-        n <- choose (0, 12)
-        i <- choose (0, 2^n-1)
-        return $ ModTest n i
-
-instance Arbitrary (ModTest 17) where
-    arbitrary = do
-        n <- choose (0, 4)
-        i <- choose (0, 2^n-1)
-        return $ ModTest n i
-
-moddft17Test :: Config -> Spec
-moddft17Test conf = prop "Generated ModDFT (p = 17)"
-    (modDFTTest conf :: ModTest 17 -> Property)
-
-moddft2013265921Test :: Config -> Spec
-moddft2013265921Test conf = prop "Generated ModDFT (p = 2013265921)"
-    (modDFTTest conf :: ModTest 2013265921 -> Property)
-
-modDFTTest :: forall p . KnownNat p => Config -> ModTest p -> Property
-modDFTTest conf (ModTest n i) = ioProperty $
-    withModularTransform conf ("moddft_" ++ show p ++ "_" ++ show n) fft_spl $ \moddft ->
-    -- We must evaluate the result now because moddft is dynamically loaded code
-    -- that cannot escape the scope of withModularTransform
-    return $! moddft e_i == res
-  where
-    fft_spl :: SPL (Exp (ℤ/p))
-    fft_spl = dit (2^n)
-
-    p :: Integer
-    p = natVal (Proxy :: Proxy p)
-
-    len :: Int
-    len = 2^n
-
-    e_i :: V.Vector (ℤ/p)
-    e_i = V.generate len $ \j -> if i == j then 1 else 0
-
-    res :: V.Vector (ℤ/p)
-    res = V.generate len $ \j -> a^j
-      where
-        a :: ℤ/p
-        a = w^i
-
-    w :: ℤ/p
-    w = omega len
-
 modCodegenTests :: Config
                 -> Spec
 modCodegenTests conf =
-    describe "Generated ModDFT" $ do
-    moddft17Test conf
-    moddft2013265921Test conf
+    describe "Generated modular DFT" $ do
+    mkModularDFTCodegenTest (Proxy :: Proxy 17) conf "DIT" dit 4
+    mkModularDFTCodegenTest (Proxy :: Proxy 2013265921) conf "DIT" dit 12
+
+mkModularDFTCodegenTest :: forall p . KnownNat p
+                        => Proxy p
+                        -> Config
+                        -> String
+                        -> (Int -> SPL (Exp (ℤ/p)))
+                        -> Int
+                        -> Spec
+mkModularDFTCodegenTest proxy conf desc mkSPL n_max =
+    describe ("Modular DFT (p=" ++ show (natVal proxy) ++ ")") $
+    sequence_ [test n | n <- [1..n_max::Int]]
+  where
+    p :: Integer
+    p = natVal proxy
+
+    test :: Int -> Spec
+    test n = it (desc ++ "(2^" ++ show n ++ ")") $ \(k :: Property -> IO Result) ->
+        withModularTransform conf ("moddft_" ++ show p ++ "_" ++ show n) fft_spl $ \moddft ->
+        -- We must evaluate the result now because moddft is dynamically loaded code
+        -- that cannot escape the scope of withModularTransform
+        k $ forAll basis $ \i -> moddft (e_i i) === res i
+      where
+        basis :: Gen Int
+        basis = do
+            Positive x <- arbitrary
+            return $ x `mod` (2^n)
+
+        fft_spl :: SPL (Exp (ℤ/p))
+        fft_spl = mkSPL (2^n)
+
+        len :: Int
+        len = 2^n
+
+        e_i :: Int -> V.Vector (ℤ/p)
+        e_i i = V.generate len $ \j -> if i == j then 1 else 0
+
+        res :: Int -> V.Vector (ℤ/p)
+        res i = V.generate len $ \j -> a^j
+          where
+            a :: ℤ/p
+            a = w^i
+
+        w :: ℤ/p
+        w = omega len
 
 -- | Generate vectors of a given size with uniformly distributed elements.
 uniformVectorsOfSize :: (Arbitrary (Uniform01 a), V.Storable a)
